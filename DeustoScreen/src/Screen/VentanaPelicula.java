05668cfbdcbe71ac7866a2190f28223efc454466
Diegogoyo1 <diego.goyoaga@opendeusto.es>
2023-10-23 22:53:19 +0200

    Intento de arreglo

--- DeustoScreen/src/Screen/VentanaPelicula.java
+++ DeustoScreen/src/Screen/VentanaPelicula.java
@@ -3,6 +3,9 @@ package Screen;
 import javax.swing.JFrame;
 
 public class VentanaPelicula extends JFrame{
-
+	public static void main1(String[] args) {
+		VentanaPelicula VP = new VentanaPelicula();
+	
+}
 	
 }
