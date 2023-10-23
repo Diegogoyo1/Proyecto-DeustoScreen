351979bfdf408f35429da7f018912c8735116446
Diegogoyo1 <diego.goyoaga@opendeusto.es>
2023-10-23 22:46:42 +0200

    Model 1 Ventana Precios

--- DeustoScreen/src/Screen/VentanaPelicula.java
+++ DeustoScreen/src/Screen/VentanaPelicula.java
@@ -3,13 +3,6 @@ package Screen;
 import javax.swing.JFrame;
 
 public class VentanaPelicula extends JFrame{
-	private void syso() {
-		// TODO Auto-generated method stub
 
-	}
-		// TODO Auto-generated method stub
-
-	}
-	}
 	
 }
