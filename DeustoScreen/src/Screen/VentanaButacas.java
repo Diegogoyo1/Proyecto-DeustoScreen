acb5ba0fab00c3c3ed8f61f4021e73850108f022
Nicolás Presa <nico.p.cueva@opendeusto.es>
2023-10-24 20:57:44 +0200

    Continuacion ventanaButacas

--- DeustoScreen/src/Screen/VentanaButacas.java
+++ DeustoScreen/src/Screen/VentanaButacas.java
@@ -1,18 +1,20 @@
 package Screen;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.border.EmptyBorder;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 
 public class VentanaButacas extends JFrame{
 	private JPanel pNorte, pSur, pEste, pOeste;
@@ -20,74 +22,120 @@ public class VentanaButacas extends JFrame{
 	private JTable tblButacas1, tblButacas2;
 	private DefaultTableModel modTblButacas1, modTblButacas2;
 	private FlowLayout layoutTabButacas1, layoutTabButacas2;
+	private JLabel lblPasillo;
+	
+	
 	
 	public VentanaButacas() {
 		
 		
+		
+		//PANELES
 		pNorte = new JPanel();
 		pSur = new JPanel();
-		pEste = new JPanel(new GridLayout(3,0));
+		pEste = new JPanel();
 		pOeste = new JPanel();
 		
 		
-
 		
-		
-		
-		
+		//POSICION DE PANELES
 		getContentPane().add(pNorte, BorderLayout.NORTH);
 		getContentPane().add(pSur, BorderLayout.SOUTH);
 		getContentPane().add(pEste, BorderLayout.EAST );
 		getContentPane().add(pOeste, BorderLayout.WEST);
 
 		
+		//LABELS
+		lblPasillo = new JLabel("P"); //¿Como poner un label en vertical?
 		
+		
+		//BOTONES
 		btnAtras = new JButton("Atras");
+		btnSiguiente = new JButton ("Siguiente");
+		
+		
+		
+		//ACCIONES DE BOTONES
 		btnAtras.addActionListener((e)-> {
-			System.exit(0);
+			dispose();
 			
 				
 			});
 		
-		btnSiguiente = new JButton ("Siguiente");
 		btnSiguiente.addActionListener((e)-> {
 			
 		});
 					
-		Object [] titulos1 = {"", "", "", "", ""};
+		
+		
+		//CREACION PRIMERA TABLA BUTACAS
+		Object [] titulos1 = {"A", "B", "C", "D", "E"};
 		modTblButacas1 = new DefaultTableModel();
 		modTblButacas1.setColumnIdentifiers(titulos1);
 		tblButacas1 = new JTable(modTblButacas1);
 		layoutTabButacas1  = new FlowLayout();
 		layoutTabButacas1.setVgap(200);
+		layoutTabButacas1.setHgap(60);
 		pOeste.setLayout(layoutTabButacas1);
 		
+		tblButacas1.getColumnModel().getColumn(0).setPreferredWidth(75);
+		tblButacas1.getColumnModel().getColumn(1).setPreferredWidth(75);
+		tblButacas1.getColumnModel().getColumn(2).setPreferredWidth(75);
+		tblButacas1.getColumnModel().getColumn(3).setPreferredWidth(75);
+		tblButacas1.getColumnModel().getColumn(4).setPreferredWidth(75);
+		tblButacas1.setRowHeight(75);
+		/*tblButacas1.setIntercellSpacing(new Dimension(10,10));  //Para dejar espacio entre celdas
+		
+		JTableHeader header = tblButacas1.getTableHeader();
+		DefaultTableCellRenderer headerRenderer = (DefaultTableCellRenderer) header.getDefaultRenderer();
+        headerRenderer.setHorizontalAlignment(SwingConstants.LEFT);*/  //Para mover los titulos a la derecha
+		
+
+
+		
 		String fila1[] =  {"01","02","03","04", "05"};
-		String fila2[] =  {"11","12","13","14", "15"};
-		String fila3[] =  {"21","22","23","24", "25"};
+		String fila2[] =  {"01","02","03","04", "05"};
+		String fila3[] =  {"01","02","03","04", "05"};
+		String fila4[] =  {"01","02","03","04", "05"};
 		modTblButacas1.addRow(fila1);
 		modTblButacas1.addRow(fila2);
 		modTblButacas1.addRow(fila3);
+		modTblButacas1.addRow(fila4);
 		
 		
 		
+		//CREACION SEGUNDA TABLA BUTACAS
 		Object [] titulos2 = {"", "", "", "", ""};
 		modTblButacas2 = new DefaultTableModel();
 		modTblButacas2.setColumnIdentifiers(titulos2);
 		tblButacas2 = new JTable(modTblButacas2);
 		layoutTabButacas2  = new FlowLayout();
 		layoutTabButacas2.setVgap(200);
+		layoutTabButacas2.setHgap(60);
 		pEste.setLayout(layoutTabButacas2);
 		
-		String fila4[] =  {"06","07","08","09", "10"};
-		String fila5[] =  {"16","17","18","19", "20"};
-		String fila6[] =  {"26","27","28","29", "30"};
-		modTblButacas2.addRow(fila4);
+		tblButacas2.getColumnModel().getColumn(0).setPreferredWidth(75);
+		tblButacas2.getColumnModel().getColumn(1).setPreferredWidth(75);
+		tblButacas2.getColumnModel().getColumn(2).setPreferredWidth(75);
+		tblButacas2.getColumnModel().getColumn(3).setPreferredWidth(75);
+		tblButacas2.getColumnModel().getColumn(4).setPreferredWidth(75);
+		tblButacas2.setRowHeight(75);
+		/*tblButacas2.setIntercellSpacing(new Dimension(10,10));*/
+		
+		String fila5[] =  {"06","07","08","09", "10"};
+		String fila6[] =  {"06","07","08","09", "10"};
+		String fila7[] =  {"06","07","08","09", "10"};
+		String fila8[] =  {"06","07","08","09", "10"};
+		
 		modTblButacas2.addRow(fila5);
 		modTblButacas2.addRow(fila6);
+		modTblButacas2.addRow(fila7);
+		modTblButacas2.addRow(fila8);
 		
 		
 		
+		
+		//ANYADIR LOS COMPONENTES A LOS PANELES
 		pOeste.add(tblButacas1);
 		pEste.add(tblButacas2);
 		pSur.add(btnAtras);
@@ -95,15 +143,16 @@ public class VentanaButacas extends JFrame{
 		
 		
 		
+		//DEFINIR LA VENTANA
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds (100,100,1000,600);
+		setBounds (0,0,1000,750);
 		setVisible(true);
 		
+
 		
 		
-		
-	
 }
+	//PRUEBA DE LA VENTANA CON MAIN
 	public static void main(String[] args) {
 		VentanaButacas VB = new VentanaButacas();	
 			
