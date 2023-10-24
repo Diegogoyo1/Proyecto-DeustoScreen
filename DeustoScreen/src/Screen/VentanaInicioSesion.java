dd3c3cb7aed098d805c3bd5432ef50e037f0eb40
GorkaOrtuzar <ortuzar.gorka@opendeusto.es>
2023-10-24 18:53:42 +0200

    Inicios sesion y registro modelo2

---
+++ DeustoScreen/src/Screen/VentanaInicioSesion.java
@@ -0,0 +1,89 @@
+package Screen;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class VentanaInicioSesion extends JFrame {
+	private JPanel pNorte,pCentro,pSur,pCentroIzquierda;
+	private JLabel lblTituloIS ,lblCorreoElectronico,lblContraseniaIs;
+	private JTextField txtCorreoElectronico;
+	private JButton btnSalir, BtnIniciarSesion;
+	private JPasswordField contraseniaIs;
+	
+	public VentanaInicioSesion() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		pNorte = new JPanel(new GridLayout(1, 2));
+		pCentro = new JPanel(new GridLayout(1, 2));
+		pSur = new JPanel();
+
+		pCentroIzquierda = new JPanel(new GridLayout(2, 1));
+		pCentro.add(pCentroIzquierda);
+		
+		getContentPane().add(pNorte, BorderLayout.NORTH);
+		getContentPane().add(pCentro, BorderLayout.CENTER);
+		getContentPane().add(pSur, BorderLayout.SOUTH);
+		
+		//Labels
+		lblCorreoElectronico = new JLabel("  Correo electronico: ");
+		lblTituloIS = new JLabel("     INICIAR SESION");
+		lblContraseniaIs = new JLabel("  Contrasenia: ");
+			
+		//TextField nuevos
+		txtCorreoElectronico= new JTextField();
+		contraseniaIs= new JPasswordField();
+		
+		// Enlazar los paneles con los label y txtField
+		
+			pCentroIzquierda.add(lblCorreoElectronico);
+			pCentroIzquierda.add(txtCorreoElectronico);
+			pCentroIzquierda.add(lblContraseniaIs);
+			pCentroIzquierda.add(contraseniaIs);
+				
+			pNorte.add(lblTituloIS);
+				
+		//botones
+		BtnIniciarSesion = new JButton("INICIAR SESION");
+		btnSalir = new JButton("SALIR");
+		pSur.add(BtnIniciarSesion);
+		pSur.add(btnSalir);
+		
+		//Eventos
+		BtnIniciarSesion.addActionListener((e)->{
+			VentanaEntradas ventanaEntradas = new VentanaEntradas();
+			ventanaEntradas.setVisible(true);
+		
+		});
+		
+		btnSalir.addActionListener((e)->{
+			System.exit(0);
+		});
+		setVisible(true);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+			VentanaInicioSesion VIS = new VentanaInicioSesion();
+			
+		
+	}
+	
+	
+		
+}	
+		
+		
+	
+
+	
