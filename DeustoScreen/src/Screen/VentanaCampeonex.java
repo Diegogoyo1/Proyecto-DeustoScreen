2e6b6dc4b453bc263a4fab1b1ec1979d8e2f3c21
ainaramaroto <ainara.maroto@opendeusto.es>
2023-10-29 19:07:04 +0100

    VentanaCampeonex

--- DeustoScreen/src/Screen/VentanaCampeonex.java
+++ DeustoScreen/src/Screen/VentanaCampeonex.java
@@ -7,35 +7,88 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class VentanaCampeonex extends JFrame {
-	protected JPanel pNorte, pSur, pEste, pOeste;
-	protected JButton btnAtras;
-	protected JLabel lblSetNombre,lblNombre,lblSetReparto, lblReparto, lblSetDuracion, lblDuracion, lblSetCategoria,lblCategoria, lblSetSinopsis;  ;
-	
+	private JPanel pNorte, pSur, pCentro, pCentroIz, pCentroDe; 
+	private JLabel lblSetNom, lblSetCat, lblSetDur, lblSetRep, lblSetSip, lblNom, lblCat, lblDur, lblRep;
+	private JButton btnA;
+	private JTextArea txtSinp;
+	//private JScrollPane scrollTxtArea;
 	
 	public VentanaCampeonex() {
 		
-		pNorte = new JPanel();
+		//CREACIÓN PANELES
+		pCentro = new JPanel (new GridLayout (1, 2));
+		pCentroIz = new JPanel (new GridLayout (5, 2));
+		pCentroDe = new JPanel();
 		pSur = new JPanel();
-		pEste = new JPanel(new GridLayout(2,0));
-		pOeste = new JPanel();
+		pNorte = new JPanel();
 		
-		getContentPane().add(pNorte, BorderLayout.NORTH);
+		getContentPane().add(pCentro, BorderLayout.CENTER);
 		getContentPane().add(pSur, BorderLayout.SOUTH);
-		getContentPane().add(pEste, BorderLayout.EAST);
-		getContentPane().add(pOeste, BorderLayout.WEST);
+		getContentPane().add(pNorte, BorderLayout.NORTH);
 		
 		
-		btnAtras = new JButton("Atras");
-		btnAtras.addActionListener((e)-> {
+		pCentro.add(pCentroIz);
+		pCentro.add(pCentroDe);
+		
+		//LABELS
+		lblSetNom = new JLabel(" NOMBRE: ");
+		lblSetCat = new JLabel (" CATEGORÍA: ");
+		lblSetDur = new JLabel(" DURACIÓN: ");
+		lblSetRep = new JLabel (" REPARTO: ");
+		lblSetSip = new JLabel (" SINOPSIS: ");
+		lblNom = new JLabel(" Campeonex ");
+		lblCat = new JLabel (" Comedia "); // poner desde la ventana
+		lblDur = new JLabel(" 2h 4mins ");
+		lblRep = new JLabel (" Elisa Hipólito ");
+		
+		
+		//TEXTAREA
+		txtSinp = new JTextArea(16, 58);
+		txtSinp = new JTextArea("Nuestro equipo de \"campeones\" abandona el baloncesto "
+				+ "para adentrarse por error en el fascinante mundo del atletismo de la "
+				+ "mano de una entrenadora novata. Entre las duras exigencias de la nueva "
+				+ "disciplina y la sorprendente capacidad de la entrenadora para atraer "
+				+ "todo tipo de desgracias y calamidades, todo acabará saliendo del revés.");
+		txtSinp.setLineWrap(true);
+		txtSinp.setWrapStyleWord(true);
+		txtSinp.setEditable(false);
+		//scrollTxtArea = new JScrollPane(txtSinp);
+		//getContentPane().add(scrollTxtArea, BorderLayout.CENTER);
+		
+		//ENLAZER PANELES CON LABELS Y TEXTFIELDS
+		pCentroIz.add(lblSetNom);
+		pCentroIz.add(lblNom);
+		pCentroIz.add(lblSetCat);
+		pCentroIz.add(lblCat);
+		pCentroIz.add(lblSetDur);
+		pCentroIz.add(lblDur);
+		pCentroIz.add(lblSetCat);
+		pCentroIz.add(lblCat);
+		pCentroIz.add(lblSetRep);
+		pCentroIz.add(lblRep);
+		pCentroIz.add(lblSetSip);
+		pCentroIz.add(txtSinp);
+		
+		
+		//BOTONES
+		btnA = new JButton("ATRÁS");
+		pSur.add(btnA);
+		
+		
+		//EVENTOS BOTONES
+		btnA.addActionListener((e) -> {
 			dispose();
-			
 		});
+		
+		
+		
 		//DEFINIR VENTANA
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds (600,300,380,400);
-		setTitle("BARBIE");
+		setTitle("CAMPEONEX");
 		setVisible(true);
 		
 	}
