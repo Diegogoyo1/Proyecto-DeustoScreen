fe6894ddd148be226aaf165a238bb2886a1f42fb
GorkaOrtuzar <ortuzar.gorka@opendeusto.es>
2023-12-15 13:36:30 +0100

    Impresion de peliculas,sala, horario y asiento

--- DeustoScreen/src/screen/VentanaPago.java
+++ DeustoScreen/src/screen/VentanaPago.java
@@ -19,10 +19,11 @@ import javax.swing.border.EmptyBorder;
 
 public class VentanaPago extends JFrame {
 	private JButton btnFinalizarCompra, btnAtras;
-	private JPanel pSur, pCentro, pNorte;
+	private JPanel pSur, pCentro, pNorte,pEste,pOeste;
 	private JFrame vActual, vAnterior;
 	private JLabel  lblTitulo,lblNumPrec, lblEntradasMenores, lblEntradasEntreanios, lblEntradasMayores, lblTotal, lblRecibo, lblGracias,
 					lblAsteriscos1, lblAsteriscos2, lblAsteriscos3, lblAsteriscos4, lblAsteriscos5;
+	//private JLabel (inicializa los valores que estan en resumen inicio pago )
 	
 	public VentanaPago(JFrame va) {
 		vActual=this;
@@ -62,7 +63,7 @@ public class VentanaPago extends JFrame {
 		
 		
 		
-		//LABELS
+		//LABELS(resumen Pago)
 		lblTitulo = new JLabel("RESUMEN PAGO");
 		
 		lblNumPrec = new JLabel("TIPO                    nº               Pr.");
@@ -123,23 +124,75 @@ public class VentanaPago extends JFrame {
 		lblTotal.setBounds(700,260,500,500);
 		
 		
-
+		// Resumen de info de peliculas(Ainara)
+		
+		/*lblTitulo = new JLabel("RESUMEN PELICULA");
+		
+		lblAsteriscos1 = new JLabel("*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *");
+		lblAsteriscos1.setFont(new Font( "Arial", Font.PLAIN, 20));
+		lblAsteriscos1.setBounds(700, -50, 500, 500);
+		
+		lblAsteriscos2 = new JLabel("*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *");
+		lblAsteriscos2.setFont(new Font( "Arial", Font.PLAIN, 20));
+		lblAsteriscos2.setBounds(700, 25, 500, 500);
+		
+		lblAsteriscos3 = new JLabel("*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *");
+		lblAsteriscos3.setFont(new Font( "Arial", Font.PLAIN, 20));
+		lblAsteriscos3.setBounds(700, 230, 500, 500);
+		
+		lblAsteriscos4 = new JLabel("*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *");
+		lblAsteriscos4.setFont(new Font( "Arial", Font.PLAIN, 20));
+		lblAsteriscos4.setBounds(700, 330, 500, 500);
+		
+		lblAsteriscos5 = new JLabel("*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *");
+		lblAsteriscos5.setFont(new Font( "Arial", Font.PLAIN, 20));
+		lblAsteriscos5.setBounds(700, 425, 500, 500);
 		
 		
-		pCentro.add(lblEntradasEntreanios);
-		pCentro.add(lblEntradasMenores);
-		pCentro.add(lblEntradasMayores);
-		pCentro.add(lblAsteriscos1);
-		pCentro.add(lblAsteriscos2);
-		pCentro.add(lblAsteriscos3);
-		pCentro.add(lblAsteriscos4);
-		pCentro.add(lblAsteriscos5);
-		pCentro.add(lblRecibo);
-		pCentro.add(lblGracias);
-		pCentro.add(lblTotal);
-		pCentro.add(lblNumPrec);
+		lblPelicula = new JLabel("PELICULA                   " + String.valueOf(VentanaSeleccionEntradas.getMayores()()) + "                " + String.valueOf(VentanaSeleccionEntradas.getMenores()*4));
+		lblPelicula.setForeground(Color.BLACK);
+		lblPelicula.setFont(new Font( "Arial", Font.PLAIN, 25));
+		lblPelicula.setBounds(700,100,500,500);
+		
+		lblSala = new JLabel("SALA                  " + String.valueOf(VentanaSeleccionEntradas.getEntreanios()) + "                " + String.valueOf(VentanaSeleccionEntradas.getEntreanios()*7));
+		lblSala.setForeground(Color.BLACK);
+		lblSala.setFont(new Font( "Arial", Font.PLAIN, 25));
+		lblSala.setBounds(700,150,500,500);
 		
 		
+		lblAsiento = new JLabel("ASIENTO                  " + String.valueOf(VentanaSeleccionEntradas.getMayores()) + "                " + String.valueOf(VentanaSeleccionEntradas.getMayores()*5));
+		lblAsiento.setForeground(Color.BLACK); 
+		lblAsiento.setFont(new Font( "Arial", Font.PLAIN, 25));
+		lblEntradasMayores.setBounds(700,200,500,500);
+		
+		lblHorario = new JLabel("HORA                 " + String.valueOf(VentanaSeleccionEntradas.getMayores()) + "                " + String.valueOf(VentanaSeleccionEntradas.getMayores()*5));
+		lblHorario.setForeground(Color.BLACK); 
+		lblHorario.setFont(new Font( "Arial", Font.PLAIN, 25));
+		lblHorario.setBounds(700,200,500,500);*/
+		
+		
+		pEste.add(lblEntradasEntreanios);
+		pEste.add(lblEntradasMenores);
+		pEste.add(lblEntradasMayores);
+		pEste.add(lblAsteriscos1);
+		pEste.add(lblAsteriscos2);
+		pEste.add(lblAsteriscos3);
+		pEste.add(lblAsteriscos4);
+		pEste.add(lblAsteriscos5);
+		pEste.add(lblRecibo);
+		pEste.add(lblGracias);
+		pEste.add(lblTotal);
+		pEste.add(lblNumPrec);
+		
+		/*pOeste.add(lblSala);
+		pOeste.add(lblAsiento);
+		pOeste.add(lblHora);
+		pOeste.add(lblAsteriscos1);
+		pOeste.add(lblAsteriscos2);
+		pOeste.add(lblAsteriscos3);
+		pOeste.add(lblAsteriscos4);
+		pOeste.add(lblAsteriscos5);
+		*/
 		pSur.add(btnAtras);
 		pSur.add(btnFinalizarCompra);
 		pNorte.add(lblTitulo);
