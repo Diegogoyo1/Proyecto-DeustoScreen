58b275093c22559312f1f7fe1d1923d82f5b5a8c
GorkaOrtuzar <ortuzar.gorka@opendeusto.es>
2024-01-11 17:10:05 +0100

    Ventana PerfilUsuario

--- DeustoScreen/src/screen/VentanaPerfilUsuario.java
+++ DeustoScreen/src/screen/VentanaPerfilUsuario.java
@@ -1,10 +1,13 @@
 package screen;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Image;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,8 +18,8 @@ import javax.swing.border.EmptyBorder;
 import domain.Usuario;
 
 public class VentanaPerfilUsuario extends JFrame {
-	private JPanel pCentro,pSur,pEste;
-	private JLabel lblNombre,lblApellido,lblFechaNaciminto,lbltlf,lblCorreoElectronico,lblPuntos,lblImagen;
+	private JPanel pCentro,pSur,pOeste;
+	private JLabel imagenUsuario, lblNombre,lblApellido,lblFechaNaciminto,lbltlf,lblCorreoElectronico,lblPuntos;
 	private JFrame vActual, vAnterior;
 	private JButton btnAtras;
 	private static Logger logger = Logger.getLogger(Main.class.getName());
@@ -28,27 +31,28 @@ public class VentanaPerfilUsuario extends JFrame {
 		//CREACIÓN PANELES
 		pCentro = new JPanel ();
 		pSur = new JPanel();
-		pEste = new JPanel();
+		pOeste = new JPanel();
 		
 		
 		getContentPane().add(pCentro, BorderLayout.CENTER);
 		getContentPane().add(pSur, BorderLayout.SOUTH);
-		getContentPane().add(pEste,BorderLayout.EAST);
-
-        //ImageIcon imagen = new ImageIcon("imagenes/foto.png");
-		//lblImagen = new JLabel(imagen);
-		lblNombre = new JLabel("NOMBRE: " + u.getNombre());
-		lblApellido = new JLabel ("APELLIDO: " +u.getApellido());
-		lblFechaNaciminto = new JLabel("FECHA DE NACIMIENTO: "+ u.getFechaNacimientoStr());
-		lbltlf = new JLabel ("TELÉFONO: " + u.getTlf());
-		lblCorreoElectronico = new JLabel("CORREO ELECTRONICO" + u.getCorreoElectronico());
-		lblPuntos = new JLabel ("PUNTOS: " +u.getContadorPuntos());
+		getContentPane().add(pOeste, BorderLayout.WEST);
+		
+		
+		imagenUsuario = new JLabel(new ImageIcon("imagenes/foto.png"));
+        imagenUsuario.setPreferredSize(new Dimension(100,100));
+		lblNombre = new JLabel("-> NOMBRE: " + u.getNombre());
+		lblApellido = new JLabel ("-> APELLIDO: " +u.getApellido());
+		lblFechaNaciminto = new JLabel("-> FECHA DE NACIMIENTO: "+ u.getFechaNacimientoStr());
+		lbltlf = new JLabel ("-> TELÉFONO: " + u.getTlf());
+		lblCorreoElectronico = new JLabel("-> CORREO ELECTRONICO" + u.getCorreoElectronico());
+		lblPuntos = new JLabel ("-> PUNTOS: " +u.getContadorPuntos());
 		
 		
 		
-		btnAtras = new JButton("Atras");
-				
-		//pEste.add(lblImagen);
+		
+			
+		pOeste.add(imagenUsuario);
 		pCentro.add(lblNombre);
 		pCentro.add(lblApellido);
 		pCentro.add(lblFechaNaciminto);
