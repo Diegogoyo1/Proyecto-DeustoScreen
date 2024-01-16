6ab42414928fb4bc97e443734f52ef8983c8b802
GorkaOrtuzar <ortuzar.gorka@opendeusto.es>
2024-01-16 17:06:25 +0100

    Ventana Pago paneles verticales

--- DeustoScreen/src/screen/VentanaPago.java
+++ DeustoScreen/src/screen/VentanaPago.java
@@ -11,6 +11,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -30,7 +31,7 @@ public class VentanaPago extends JFrame {
 	private JButton btnFinalizarCompra, btnAtras;
 	private JPanel pSur, pCentro, pNorte,pEntradas,pRecibo;
 	private JFrame vActual, vAnterior;
-	private JLabel  lblTituloRecibo,lblNumPrec, lblEntradasMenores, lblEntradasEntreanios, lblEntradasMayores, lblTotal, lblRecibo, lblGracias,
+	private JLabel  lblDescuento,lblPagoFinal, lblEntradasMenores, lblEntradasEntreanios, lblEntradasMayores, lblTotal, lblRecibo, lblGracias,
 					lblAsteriscos1, lblAsteriscos2, lblAsteriscos3, lblAsteriscos4, lblAsteriscos5;
 	private JLabel  lblSala, lblHora, lblAsientos, lblPelicula, lblNumEntradas, lblEntradas,
 					lblAsteriscos6, lblAsteriscos7, lblAsteriscos8;
@@ -51,9 +52,10 @@ public class VentanaPago extends JFrame {
 		pSur = new JPanel();
 		pCentro = new JPanel(new GridLayout(1,2));
 		pNorte = new JPanel();
-		pEntradas = new JPanel();
-		pRecibo = new JPanel();
-
+		pEntradas = new JPanel(/*new BoxLayout(pEntradas, BoxLayout.Y_AXIS)*/);
+		pRecibo = new JPanel(/*new BoxLayout(pRecibo, BoxLayout.Y_AXIS)*/);
+		
+		
 		asientos = "";
 		for(String a: asientosSeleccionados) {
 			asientos = asientos+a+" ";
@@ -108,11 +110,11 @@ public class VentanaPago extends JFrame {
 			
 		});
 		
-		lblAsteriscos1 = new JLabel("");
+		lblAsteriscos1 = new JLabel("***************************************************");
 		lblAsteriscos1.setFont(new Font( "Arial", Font.PLAIN, 20));
 		lblEntradas = new JLabel("ENTRADAS");
 		lblEntradas.setFont(new Font( "Arial", Font.PLAIN, 20));
-		lblAsteriscos2 =new JLabel("*");
+		lblAsteriscos2 =new JLabel("***************************************************");
 		lblAsteriscos2.setFont(new Font( "Arial", Font.PLAIN, 20));
 		lblPelicula = new JLabel("Pelicula:   "  + String.valueOf(VentanaEntradas.getComboTitulos()));
 		lblPelicula.setFont(new Font( "Arial", Font.PLAIN, 20));
@@ -120,9 +122,9 @@ public class VentanaPago extends JFrame {
 		lblHora.setFont(new Font( "Arial", Font.PLAIN, 20));
 		lblNumEntradas = new JLabel("Nº Entradas:   " + String.valueOf(VentanaSeleccionEntradas.getTotalEntradas()));
 		lblNumEntradas.setFont(new Font( "Arial", Font.PLAIN, 20));
-		lblSala = new JLabel("Sala"   + sala);
+		lblSala = new JLabel("Sala: "   + sala);
 		lblSala.setFont(new Font( "Arial", Font.PLAIN, 20));
-		lblAsientos = new JLabel("Asientos " + asientos );
+		lblAsientos = new JLabel("Asientos: " + asientos );
 		lblAsientos.setFont(new Font("Arial", Font.PLAIN,20));
 		
 		pEntradas.add(lblAsteriscos1);
@@ -134,26 +136,33 @@ public class VentanaPago extends JFrame {
 		pEntradas.add(lblSala);
 		pEntradas.add(lblAsientos);
 		pEntradas.add(Box.createVerticalStrut(60));
-		pEntradas.setBorder(new EmptyBorder(150,150,150,150));
+		pEntradas.setBorder(new EmptyBorder(150,300,300,300));
+
 
 		pCentro.add(pEntradas);
 		
-		lblAsteriscos3 = new JLabel("*");
+		lblAsteriscos3 = new JLabel("***************************************************");
 		lblAsteriscos3.setFont(new Font( "Arial", Font.PLAIN, 20));
 		lblRecibo = new JLabel("RECIBO");
 		lblRecibo.setFont(new Font( "Arial", Font.PLAIN, 20));
-		lblAsteriscos4 =new JLabel("");
+		lblAsteriscos4 =new JLabel("***************************************************");
 		lblAsteriscos4.setFont(new Font( "Arial", Font.PLAIN, 20));
-		lblEntradasMenores = new JLabel("Junior:   "  + String.valueOf(VentanaSeleccionEntradas.getMenores()) +"   "+ String.valueOf(VentanaSeleccionEntradas.getMenores()*4));
+		lblEntradasMenores = new JLabel("Junior:   "  + String.valueOf(VentanaSeleccionEntradas.getMenores()) +"          "+ String.valueOf(VentanaSeleccionEntradas.getMenores()*4));
 		lblEntradasMenores.setFont(new Font( "Arial", Font.PLAIN, 20));
-		lblEntradasEntreanios = new JLabel("Adulto:   " + String.valueOf(VentanaSeleccionEntradas.getEntreanios())+"   " + String.valueOf(VentanaSeleccionEntradas.getEntreanios()*7));
+		lblEntradasEntreanios = new JLabel("Adulto:   " + String.valueOf(VentanaSeleccionEntradas.getEntreanios())+"          " + String.valueOf(VentanaSeleccionEntradas.getEntreanios()*7));
 		lblEntradasEntreanios.setFont(new Font( "Arial", Font.PLAIN, 20));
-		lblEntradasMayores = new JLabel("Senior:   " + String.valueOf(VentanaSeleccionEntradas.getMayores()) + "   "+String.valueOf(VentanaSeleccionEntradas.getMayores()*5));
+		lblEntradasMayores = new JLabel("Senior:   " + String.valueOf(VentanaSeleccionEntradas.getMayores()) + "          "+String.valueOf(VentanaSeleccionEntradas.getMayores()*5));
 		lblEntradasMayores.setFont(new Font( "Arial", Font.PLAIN, 20));
-		lblAsteriscos5 = new JLabel("*");
+		lblAsteriscos5 = new JLabel("***************************************************");
 		lblAsteriscos5.setFont(new Font( "Arial", Font.PLAIN, 20));
 		lblTotal = new JLabel("TOTAL:  " + String.valueOf((VentanaSeleccionEntradas.getMenores()*4)  + (VentanaSeleccionEntradas.getEntreanios()*7) + (VentanaSeleccionEntradas.getMayores()*5)));
-		lblTitulo.setFont(new Font( "Arial", Font.PLAIN, 20));
+		lblTotal.setFont(new Font( "Arial", Font.PLAIN, 20));
+		lblAsteriscos6 = new JLabel("***************************************************");
+		lblAsteriscos6.setFont(new Font( "Arial", Font.PLAIN, 20));
+		lblDescuento = new JLabel("Descuento: " /* + VentanaPuntos.puntosSpinner() * 0,01*/);
+		lblDescuento.setFont(new Font( "Arial", Font.PLAIN, 20));
+		lblPagoFinal = new JLabel("Pago Final: "  /* + VentanaPuntos.puntosSpinner()*/);
+		lblPagoFinal.setFont(new Font( "Arial", Font.PLAIN, 20));
 
 		
 		pRecibo.add(lblAsteriscos3);
@@ -163,10 +172,13 @@ public class VentanaPago extends JFrame {
 		pRecibo.add(lblEntradasEntreanios);
 		pRecibo.add(lblEntradasMayores);
 		pRecibo.add(Box.createVerticalStrut(60));
-		pRecibo.setBorder(new EmptyBorder(150,150,150,150));
-
 		pRecibo.add(lblAsteriscos5);
 		pRecibo.add(lblTotal);
+		pRecibo.add(lblAsteriscos6);
+		pRecibo.add(lblDescuento);
+		pRecibo.add(lblPagoFinal);
+		pRecibo.setBorder(new EmptyBorder(150,300,300,300));
+
 
 		pCentro.add(pRecibo);
 		
